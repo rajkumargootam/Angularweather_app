917b7fdf4101fccad2ca478f19181a56efc81d91
rajkumar <rajkumar.gootam@gmail.com>
2016-05-20 11:53:57 +0530

    direct calling -273 in index we can call it in controller using promise

--- app.js
+++ app.js
@@ -20,8 +20,14 @@ angularApp.controller("HomeController",function($resource){
     console.log(vm.cityName);
     var weatherResource = $resource('http://api.openweathermap.org/data/2.5/weather',
       {q:vm.cityName,appid:'27d43832d2a4adcb97fcbfa23db130aa'});
-    vm.weatherResponse = weatherResource.get();
-    console.log(vm.weatherResponse);
+    vm.weather = weatherResource.get();
+    vm.weather.$promise.then(function(data){
+      vm.weatherResponse = data;
+      vm.weatherResponse.main.temp -= 273;
+      console.log(vm.weatherResponse)
+    }, function(error){
+      console.log(error);
+    });
    }
 });
 
